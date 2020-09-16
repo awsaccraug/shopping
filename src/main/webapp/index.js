bd005117407b20885c2f5b4d420208314dcdb1f8
Emmanuel Koomson <emmanuelkoomson.forever@gmail.com>
2020-09-16 21:13:42 +0000

    updates

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -410,7 +410,7 @@ function printOrderList() {
         headers: {Authorization: "Bearer " + token}
     }).done(function (orderList) {
         console.log("ORDER LIST PRINTED");
-	console.log(orderList.length);
+	console.log(orderList);
         bildHtmlOrderListRows(orderList);
     }).fail(function () {
         console.log("ORDER LIST NOT PRINTED");
