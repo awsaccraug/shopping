2f98eef3b5b51a31cbeaeb5fc3f01003cde4c135
Emmanuel Koomson <emmanuelkoomson.forever@gmail.com>
2020-10-04 17:54:58 +0000

    updates

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -72,9 +72,9 @@ $(function () {
     // [Add to Cart]
 });
 
-function showCartNotification() {
+function showCartNotification(productName) {
     $("#mar-showCart").notify(
-     "tada  added to cart", 
+     productName+ " added to cart", 
     { position:"bottom" }
 );
 }
@@ -125,7 +125,7 @@ function bildHtmlProductsRows(products, rowsPerPage) {
         html += ' <td class="text-right">' + products[i].price.toLocaleString() + '</td>';
         html += ' <td class="text-right">';
         html += '  <a href="#" class="nav-link btn btn-info btn-sm" ' +
-            'onclick="jamam(' + products[i].id + ');showCartNotification();">';
+            'onclick="jamam(' + products[i].id + ');showCartNotification(' + products[i].name + ');">';
         html += '   <span class="glyphicon glyphicon-shopping-cart"></span> Add to Cart</a>';
         html += ' </td>';
         html += '</tr>';
